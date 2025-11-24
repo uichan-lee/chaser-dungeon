91b40dba26dfaba4f9f0e07341b9074cd2d40214
aeswara-rgb <aeswara@berkeley.edu>
2025-11-23 19:58:03 -0800

    Update Main.java

--- proj5/src/core/Main.java
+++ proj5/src/core/Main.java
@@ -1,10 +1,20 @@
 package core;
 
+import tileengine.TERenderer;
+import tileengine.TETile;
+
 public class Main {
     public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        int WIDTH = 80;
+        int HEIGHT = 40;
 
-        // build your own world!
+        ter.initialize(WIDTH, HEIGHT);
 
+       
+        long seed = 123456789L;
 
+        TETile[][] world = World.generateWorld(seed);
+        ter.renderFrame(world);
     }
 }
